10562e2abee9bd4f9081ba6c64d498bd755900c5
Adrian Alcocer <jadrianalcocer11@gmail.com>
2025-03-04 01:33:46 -0600

    feat: add ms-auth microservice structure

--- ms-auth/src/main/java/com/reactor/ms_auth/MsAuthApplication.java
+++ ms-auth/src/main/java/com/reactor/ms_auth/MsAuthApplication.java
@@ -2,8 +2,10 @@ package com.reactor.ms_auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
+@EnableWebFlux
 public class MsAuthApplication {
 
 	public static void main(String[] args) {
