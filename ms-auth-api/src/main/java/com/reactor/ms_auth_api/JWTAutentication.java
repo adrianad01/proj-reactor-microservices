073a55dad5fe59b8b515cefea48fef0113b81f13
Adrian Alcocer <jadrianalcocer11@gmail.com>
2025-03-05 16:59:59 -0600

    feat: add dependencies to pom.mxl

---
+++ ms-auth-api/src/main/java/com/reactor/ms_auth_api/JWTAutentication.java
@@ -0,0 +1,4 @@
+package com.reactor.ms_auth_api;
+
+public class JWTAutentication {
+}
---
+++ ms-auth-api/src/test/java/com/reactor/ms_auth_api/MsAuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactor.ms_auth_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAuthApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
